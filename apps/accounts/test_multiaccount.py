3dc4626cf8bd20bb3696f7d008dca24268a9c517
shipovskijkorp <168233759+shipovskijkorp@users.noreply.github.com>
2026-09-22 23:12:35 +0300

    Test persistent multi-account switching

---
+++ apps/accounts/test_multiaccount.py
@@ -0,0 +1,83 @@
+from django.contrib.auth import SESSION_KEY
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import User
+
+
+class MultiAccountTests(TestCase):
+    password = "Stalingram-test-1945"
+
+    def setUp(self):
+        self.first = User.objects.create_user(
+            username="first",
+            email="first@example.com",
+            password=self.password,
+        )
+        self.second = User.objects.create_user(
+            username="second",
+            email="second@example.com",
+            password=self.password,
+        )
+        self.client.force_login(self.first)
+
+    def test_add_account_keeps_first_session_and_switches_to_second(self):
+        response = self.client.post(
+            reverse("accounts:add_account"),
+            {
+                "username": self.second.username,
+                "password": self.password,
+            },
+        )
+        self.assertRedirects(response, reverse("messenger:home"))
+
+        session = self.client.session
+        self.assertEqual(int(session[SESSION_KEY]), self.second.pk)
+        account_ids = {
+            int(item["user_id"])
+            for item in session["stalingram_accounts_v1"]
+        }
+        self.assertEqual(account_ids, {self.first.pk, self.second.pk})
+
+    def test_switch_account_does_not_require_password_again(self):
+        self.client.post(
+            reverse("accounts:add_account"),
+            {
+                "username": self.second.username,
+                "password": self.password,
+            },
+        )
+
+        response = self.client.post(
+            reverse("accounts:switch_account", args=[self.first.pk])
+        )
+        self.assertRedirects(response, reverse("messenger:home"))
+        self.assertEqual(int(self.client.session[SESSION_KEY]), self.first.pk)
+
+    def test_logout_removes_only_current_account_when_another_slot_exists(self):
+        self.client.post(
+            reverse("accounts:add_account"),
+            {
+                "username": self.second.username,
+                "password": self.password,
+            },
+        )
+
+        response = self.client.post(reverse("accounts:logout"))
+        self.assertRedirects(response, reverse("messenger:home"))
+        self.assertEqual(int(self.client.session[SESSION_KEY]), self.first.pk)
+        remaining = self.client.session["stalingram_accounts_v1"]
+        self.assertEqual([int(item["user_id"]) for item in remaining], [self.first.pk])
+
+    def test_sidebar_lists_saved_accounts(self):
+        self.client.post(
+            reverse("accounts:add_account"),
+            {
+                "username": self.second.username,
+                "password": self.password,
+            },
+        )
+        response = self.client.get(reverse("messenger:home"))
+        self.assertContains(response, "@first")
+        self.assertContains(response, "@second")
+        self.assertContains(response, "Добавить аккаунт")
