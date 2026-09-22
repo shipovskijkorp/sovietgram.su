89f9f64c70d28bb930205b2736500b1206fd3e34
shipovskijkorp <168233759+shipovskijkorp@users.noreply.github.com>
2026-09-22 22:04:37 +0300

    Harden uploaded media validation

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import Http404
 from django.urls import include, path
+
+def _deny_private_message_media(request, path):
+    raise Http404
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,4 +15,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += [
+        path("media/messages/<path:path>", _deny_private_message_media),
+    ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
